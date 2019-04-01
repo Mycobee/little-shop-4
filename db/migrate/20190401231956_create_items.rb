85fb9c56040edff62926d20f2336538adb06d3d1
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-01 17:31:36 -0600

    Setup migrations for database
    Co-authored-by: Zach Leach <35751823+zachlp2016@users.noreply.github.com>
    Co-authored-by: David Hardy <42919604+davehardy632@users.noreply.github.com>
    Co-authored-by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

---
+++ db/migrate/20190401231956_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.string :image_url, default: "https://emblemsbf.com/img/77148.jpg"
+      t.integer :quantity
+      t.float :base_price
+      t.boolean :enabled
+
+      t.timestamps
+    end
+  end
+end
