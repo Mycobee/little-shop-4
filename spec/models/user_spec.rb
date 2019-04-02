750e55e44606b69d5b7c441338d0a9f738a6c6ad
Zach Leach <zachleach@techoverco.com>
2019-04-01 19:55:50 -0600

    Added test for user spec.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_require'
+
+RSpec.describe User, type: :model do
+  describe "Relationship" do
+    it {should have_many :items}
+    it {should have_many :orders}
+
+    #verify that users don't need items through order_items
+  end
+
+  describe "Validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password_digest}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip_code}
+    it {should validate_presence_of :role}
+    it {should validate_presence_of :enabled}
+  end
+end
