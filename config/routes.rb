7bca3841c2feb2ca44c508d78f129392da6bfb0a
Zach Leach <zachleach@techoverco.com>
2019-04-02 18:45:24 -0600

    Added routes for home,items,merchants, and shopping_cart.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  resources :items, only:[:index] do
+  end
+
+  resources :merchants, only:[:index] do
+  end
+
+  resources :cart, only:[:index] do
+  end
+
+  resources :cart, only:[:index] do
+  end
+
 end
