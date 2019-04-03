2c6a704e7b87ace15fbfadb742376942be8bf2b0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-04-03 15:32:56 -0600

    Passing user and visitor navbar tests Co-authored-by: David Hardy <42919604+davehardy632@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def show
+    require 'pry'; binding.pry
     @user = User.find(params[:id])
+
   end
 
   def new
+    require 'pry'; binding.pry
+  end
 
+  def create
+    require 'pry'; binding.pry
   end
 
 end
