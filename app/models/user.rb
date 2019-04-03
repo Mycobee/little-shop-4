2af59e90507e394c736d7c0a85d2bad1eae85c58
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-03 14:56:50 -0600

    Registration Email must be unique Closes #71
    Co-authored-by: Zach Leach <35751823+zachlp2016@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   has_many :items
   has_secure_password
   validates_presence_of :name
-  validates_presence_of :email
+  validates :email, uniqueness: true, presence: true
   validates_presence_of :password
   validates_presence_of :address
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip_code
-  
+
 end
