70180dbf2811681ef0dee4eff82f59388ef3e838
David Hardy <davehardy632@gmail.com>
2019-04-04 15:09:08 -0600

    Logout functionality built expect for removing shopping cart items. shopping cart functionality not yet built.
    Co-authored-by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- spec/features/user/logout_spec.rb
+++ spec/features/user/logout_spec.rb
@@ -1,18 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'As a registered user' do
-    describe 'When i visit the logout path' do     
-        it 'redirects me to the root path' do
+RSpec.describe 'As all registered users' do
+    describe 'When i visit the logout path' do
+        it 'redirects user to the root path' do
             reg_user = create(:user)
             visit login_path
-            
-            fill_in 'Email', with: reg_user.email 
-            fill_in 'Password', with: reg_user.password 
+
+            fill_in 'Email', with: reg_user.email
+            fill_in 'Password', with: reg_user.password
             click_button("Log In")
             expect(current_path).to eq(profile_path)
             click_link("Log Out")
             expect(current_path).to eq(root_path)
             expect(page).to have_content("You are logged out")
         end
+
+        it 'redirects merchant to the root path' do
+            merchant = create(:merchant)
+            visit login_path
+
+            fill_in 'Email', with: merchant.email
+            fill_in 'Password', with: merchant.password
+            click_button("Log In")
+            expect(current_path).to eq(merchant_dashboard_path)
+            click_link("Log Out")
+            expect(current_path).to eq(root_path)
+            expect(page).to have_content("You are logged out")
+        end
+
+        it 'redirects admin to the root path' do
+            admin = create(:admin)
+            visit login_path
+
+            fill_in 'Email', with: admin.email
+            fill_in 'Password', with: admin.password
+            click_button("Log In")
+            expect(current_path).to eq(admin_dashboard_path)
+            click_link("Log Out")
+            expect(current_path).to eq(root_path)
+            expect(page).to have_content("You are logged out")
+        end
+        #COME BACK TO REST OF THIS STORY WHEN SHOPPING CART IS BUILT
     end
 end
