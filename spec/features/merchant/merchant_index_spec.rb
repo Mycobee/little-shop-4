2e987f0adac38eead7c9617b5f33e0f872063612
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-04-04 10:51:55 -0600

    Created merchant index functionality, and controller functionality

--- spec/features/merchant/merchant_index_spec.rb
+++ spec/features/merchant/merchant_index_spec.rb
@@ -6,27 +6,32 @@ RSpec.describe 'As a visitor viewing the merchants index' do
             @merchant_1 =  create(:merchant)
             @merchant_2 =  create(:merchant)
             @merchant_3 =  create(:merchant)
+            @merchant_4 = create(:inactive_merchant)
+            @admin = create(:admin)
         end
         
         
         it 'shows every merchant name, city, and address' do
             visit merchants_path 
-            
-            expect(page).to have_content(@merchant_1.name)
-            expect(page).to have_content(@merchant_2.name)
-            expect(page).to have_content(@merchant_3.name)
+            save_and_open_page
+            expect(page).to have_content("Name: #{@merchant_1.name}")
+            expect(page).to have_content("Name: #{@merchant_2.name}")
+            expect(page).to have_content("Name: #{@merchant_3.name}")
 
-            expect(page).to have_content(@merchant_1.city)
-            expect(page).to have_content(@merchant_2.city)
-            expect(page).to have_content(@merchant_3.city)
+            expect(page).to have_content("City: #{@merchant_1.city}")
+            expect(page).to have_content("City: #{@merchant_2.city}")
+            expect(page).to have_content("City: #{@merchant_3.city}")
 
-            expect(page).to have_content(@merchant_1.state)
-            expect(page).to have_content(@merchant_2.state)
-            expect(page).to have_content(@merchant_3.state)
+            expect(page).to have_content("State: #{@merchant_1.state}")
+            expect(page).to have_content("State: #{@merchant_2.state}")
+            expect(page).to have_content("State: #{@merchant_3.state}")
 
-            expect(page).to have_content(@merchant_1.created_at)
-            expect(page).to have_content(@merchant_2.created_at)
-            expect(page).to have_content(@merchant_3.created_at)
+            expect(page).to have_content("Registration Date: #{@merchant_1.created_at}")
+            expect(page).to have_content("Registration Date: #{@merchant_2.created_at}")
+            expect(page).to have_content("Registration Date: #{@merchant_3.created_at}")
+
+            expect(page).to_not have_content(@merchant_4.name)
+            expect(page).to_not have_content(@admin.name)
         end
     end
 end
