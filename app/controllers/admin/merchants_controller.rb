964f0326bf96e119ea7b3d9e002efaf71165c52f
Zach Leach <zachleach@techoverco.com>
2019-04-07 11:10:59 -0600

    Added test for redirect of former merchant and verified it's working.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,5 +2,8 @@ class Admin::MerchantsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.merchant? == false
+      redirect_to admin_user_path(@user)
+    end
   end
 end
