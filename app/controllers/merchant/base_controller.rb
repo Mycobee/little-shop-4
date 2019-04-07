ecbbf5dd18e905d9f2a6172556fb80a9d1412fad
Zach Leach <zachleach@techoverco.com>
2019-04-06 18:41:26 -0600

    Added test for show page for admin and verified it is working.

--- app/controllers/merchant/base_controller.rb
+++ app/controllers/merchant/base_controller.rb
@@ -1,6 +1,14 @@
 class Merchant::BaseController < ApplicationController
-  before_action :require_merchant
+  before_action :require_merchant, :require_admin
+
+
   def require_merchant
     render file:"/public/404" unless current_merchant?
+  end
+
+  def require_admin
+    if current_user.admin?
+      render file:"/admin/merchants/#{current_user.id}"
+    end
   end
 end
--- spec/features/admin/merchant_show_page_spec.rb
+++ spec/features/admin/merchant_show_page_spec.rb
@@ -1,29 +1,49 @@
 RSpec.describe 'Admin visits a merchants index page, clicks and sees merchant' do
   context 'As an Admin' do
 
-    before :each do
-      @merchant_1 = create(:merchant)
-      @merchant_2 = create(:merchant)
-      @merchant_3 = create(:merchant)
-      @admin = create(:admin)
+    it 'visits the index page of merchants and sees link to merchant profile page' do
+
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_3 = create(:merchant)
+
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to \
+      receive(:current_user).and_return(admin)
 
 
 
-    end
-
-    it 'visits the index page of merchants and sees merchant info' do
 
       visit merchants_path
 
 
-      expect(page).to have_content("#{@merchant_1.name}")
+      expect(page).to have_content("#{merchant_1.name}")
+      expect(page).to have_content("#{merchant_1.name}")
+      expect(page).to have_content("#{merchant_1.city}")
+      expect(page).to have_content("#{merchant_1.state}")
 
-      click_link @merchant_1.name
-      save_and_open_page
+      click_link merchant_2.name
 
-      expect(page).to have_content("#{@merchant_1.name}")
-      expect(page).to have_content("#{@merchant_1.city}")
-      expect(page).to have_content("#{@merchant_1.state}")
+
+      expect(current_path).to eq(admin_merchant_path(merchant_2))
+
+    end
+
+    it 'clicks link and sees the merchant show page' do
+
+      merchant_2 = create(:merchant)
+
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to \
+      receive(:current_user).and_return(admin)
+
+      visit merchants_path
+
+      click_link merchant_2.name
+
+      expect(page).to have_content("#{merchant_2.name}")
     end
   end
 end
