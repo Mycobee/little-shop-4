d1c90ec934213228f745e80fa6d5fded538c9b48
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-08 11:17:19 -0600

    Added Test for item show page

--- spec/features/item/show_spec.rb
+++ spec/features/item/show_spec.rb
@@ -3,25 +3,32 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   describe "when visit item show page" do
     before :each do
-      @merch_1 = create(:user, role: "merchant", id: 1)
+      @user = create(:user)
+      @merch_1 = create(:user, role: "merchant")
       @item_1 = @merch_1.items.create(name:"Item 1", description: "cool", quantity: 50, base_price: 3.0 )
-      @order = create(:order)
-      @order_item = create(:order_item, created_at: 1.hour.ago, updated_at: 30.mins.ago)
+      @order_1 = create(:order, status: "shipped_order", user: @user)
+      @order_2 = create(:order, status: "shipped_order", user: @user)
+      @order_3 = create(:order, status: "shipped_order", user: @user)
+      @order_4 = create(:order, status: "shipped_order", user: @user)
 
-      
-      binding.pry
+      @order_item_1 = create(:order_item, order: @order_1, item: @item_1, fulfilled: true, created_at: 1.hour.ago, updated_at: 30.minutes.ago)
+      @order_item_2 = create(:order_item, order: @order_2, item: @item_1, fulfilled: true, created_at: 2.hour.ago, updated_at: 45.minutes.ago)
+      @order_item_3 = create(:order_item, order: @order_3, item: @item_1,  fulfilled: true, created_at: 3.hour.ago, updated_at: 15.minutes.ago)
+      @order_item_4 = create(:order_item, order: @order_4, item: @item_1,  fulfilled: true, created_at: 14.hour.ago, updated_at: 25.minutes.ago)
 
       visit item_path(@item_1)
     end
 
-    it "should display itme info" do
-      binding.pry
+    it "should display item info" do
+
       expect(page).to have_content("#{@item_1.name}")
-      expect(page).to have_content("#{@item_1.description}")
+      expect(page).to have_content("Description: #{@item_1.description}")
       expect(page).to have_css("img[src*='#{@item_1.image_url}']")
-      expect(page).to have_content("#{@item_1.merchant.name}")
-      expect(page).to have_content("#{@merch_1.name}")
-      expect(page).to have_content("#{@merch_1.name}")
+      expect(page).to have_content("Merchant: #{@item_1.merchant_name}")
+      expect(page).to have_content("Amount: #{@item_1.quantity}")
+      expect(page).to have_content("Price: $#{@item_1.base_price}")
+      expect(page).to have_content("Average Fulfillment Time: #{@item_1.avg_f_time}")
+      expect(page).to have_button("Add to Cart")
     end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,12 +16,29 @@ RSpec.describe Item, type: :model do
   end
 
   describe "Instance Methods" do
-    
+
     it '.merchant_name' do
       merchant = User.create!(id: 1, name: 'bob', email: 'bob@bob', address: 'this is it', city: 'jackson', state: 'MS', zip_code: 39211, password: 'password', role: 1)
       item = merchant.items.create!(name: "thing1", description: "do a thing", image_url: "https://picsum.photos/200/300?image=1", base_price: 3.50, quantity: 8)
-      
+
       expect(item.merchant_name).to eq(merchant.name)
+    end
+
+    it ".avg_f_time" do
+      @user = create(:user)
+      @merch_1 = create(:user, role: "merchant")
+      @item_1 = @merch_1.items.create(name:"Item 1", description: "cool", quantity: 50, base_price: 3.0 )
+      @order_1 = create(:order, status: "shipped_order", user: @user)
+      @order_2 = create(:order, status: "shipped_order", user: @user)
+      @order_3 = create(:order, status: "shipped_order", user: @user)
+      @order_4 = create(:order, status: "shipped_order", user: @user)
+
+      @order_item_1 = create(:order_item, order: @order_1, item: @item_1, fulfilled: true, created_at: 5.days.ago, updated_at: 30.minutes.ago)
+      @order_item_2 = create(:order_item, order: @order_2, item: @item_1, fulfilled: true, created_at: 2.hour.ago, updated_at: 45.minutes.ago)
+      @order_item_3 = create(:order_item, order: @order_3, item: @item_1,  fulfilled: true, created_at: 3.hour.ago, updated_at: 15.minutes.ago)
+      @order_item_4 = create(:order_item, order: @order_4, item: @item_1,  fulfilled: true, created_at: 14.hour.ago, updated_at: 25.minutes.ago)
+
+      expect(@item_1.avg_f_time).to include("1 day 10:16")
     end
   end
 end
