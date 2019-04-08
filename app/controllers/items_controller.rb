c919bba793f5b4966bd752c0274137e3718dc17f
Zach Leach <zachleach@techoverco.com>
2019-04-08 12:17:45 -0600

    Added test that a user sees a message to register if not a current user and verified its working.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
 
   def index
+    if current_user == nil
+      flash[:notice] = "You must register or login to finish checking out."
+    end
     @items = Item.all
   end
 
   def show
     @item = Item.find(params[:id])
   end
-
 end
